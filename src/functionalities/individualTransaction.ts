ed9f1920da6b47bddea3faa7cef0f87c3a9618ec
katta soumya <kattasoumya2002@gmail.com>
2024-09-26 13:12:54 +0530

    Implement the functionality when the account balance is 0

--- src/functionalities/individualTransaction.ts
+++ src/functionalities/individualTransaction.ts
@@ -2,13 +2,16 @@ export function individualTransaction(type: string, amount: number) {
   var currentAmount = 5000;
   switch (type) {
     case "debit":
-      if(currentAmount > amount){
-         return `current balence: ${currentAmount - amount}`;
-      } 
-      if(currentAmount < amount){
-        return `Unable to make the transaction as current balance is less than the debited amount`
+      if (currentAmount > amount) {
+        return `current balence: ${currentAmount - amount}`;
       }
-   case "credit":
+      if (currentAmount < amount && currentAmount != 0) {
+        return `Unable to make the transaction as current balance is less than the debited amount`;
+      }
+
+      return `Unable to make transaction as current account balance is 0`;
+
+    case "credit":
       return `current balence: ${currentAmount + amount}`;
   }
 }
