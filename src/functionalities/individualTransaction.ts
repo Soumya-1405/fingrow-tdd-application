2fa5f749bed560a814559c389bb256dc263af2aa
katta soumya <kattasoumya2002@gmail.com>
2024-09-26 12:32:05 +0530

    Update the functionality to return current balence when data gets credited

--- src/functionalities/individualTransaction.ts
+++ src/functionalities/individualTransaction.ts
@@ -1,6 +1,10 @@
-const data = [{ username: "souya", password: 12456 }];
-
 export function individualTransaction(type: string, amount: number) {
-    let currenAmount = 5000
-    return `current balence: ${currenAmount-amount}`;
+  let currenAmount = 5000;
+  switch (type) {
+    case "debit":
+      return `current balence: ${currenAmount - amount}`;
+
+    case "credit":
+      return `current balence: ${currenAmount + amount}`;
+  }
 }
