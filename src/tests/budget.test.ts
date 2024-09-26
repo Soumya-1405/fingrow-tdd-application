3c094f563aaec48f9d2cd262eb0c48189ff41cac
katta soumya <kattasoumya2002@gmail.com>
2024-09-27 00:57:49 +0530

    Write a test case when we set budget greater than income

--- src/tests/budget.test.ts
+++ src/tests/budget.test.ts
@@ -4,5 +4,8 @@ describe("Should set and track the budgets different categories",()=>{
     test("should set the budget for specific category", ()=>{
         expect(Budget('groceries', 2500)).toBe(`groceries budget:${2500}`)
     })
-
+    test("should show a message when we set budget more than the currentIncome",()=>{
+        expect(Budget('rent', 27000)).toBe(`Unable to set the budget as budegt is more than our income`)
+        })
+    
 })
