ebd61b27993e15c1e6454ed04f591db8a9f58e2c
katta soumya <kattasoumya2002@gmail.com>
2024-09-26 12:23:13 +0530

    Update function when amount gets debited from account

--- src/functionalities/individualTransaction.ts
+++ src/functionalities/individualTransaction.ts
@@ -1,5 +1,6 @@
 const data = [{ username: "souya", password: 12456 }];
 
 export function individualTransaction(type: string, amount: number) {
-  return 
+    let currenAmount = 5000
+    return `current balence: ${currenAmount-amount}`;
 }
