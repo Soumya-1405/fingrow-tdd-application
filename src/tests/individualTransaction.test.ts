0d99bc6d4fad16ea136208faecfb07139ce184db
katta soumya <kattasoumya2002@gmail.com>
2024-09-26 12:56:40 +0530

    Write the test case when debited amount is more than the account balance

--- src/tests/individualTransaction.test.ts
+++ src/tests/individualTransaction.test.ts
@@ -6,4 +6,7 @@ describe('Should get the transactions of the a user', () => {
     test('should get the current balance when the amounts gets credited', ()=>{
       expect(individualTransaction("credit", 3000)). toBe(`current balence: ${8000}`)
     })
+    test('should show the message when we debit money when we have debited amount more than account balance', ()=>{
+      expect(individualTransaction("debit",8000)). toBe(`Unable to make the transaction as current balance is less than the debited amount`)
+    })
   });
