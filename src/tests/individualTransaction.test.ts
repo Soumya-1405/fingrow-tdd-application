4b09f2bb103464e4100afe2e124a5102e740b7da
katta soumya <kattasoumya2002@gmail.com>
2024-09-26 12:26:31 +0530

    Write the test to see the transaction when money gets credited

--- src/tests/individualTransaction.test.ts
+++ src/tests/individualTransaction.test.ts
@@ -3,4 +3,7 @@ describe('Should get the transactions of the a user', () => {
     test('should show the current balance when the amount gets debited', () => {
       expect(individualTransaction("debit",2000)).toBe(`current balence: ${3000}`)
     });
+    test('should get the current balance when the amounts gets credited', ()=>{
+      expect(individualTransaction("credit", 3000)). toBe(`current balence: ${8000}`)
+    })
   });
