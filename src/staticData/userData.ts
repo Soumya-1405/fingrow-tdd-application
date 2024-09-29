e46abe812942301356dc1e7102aad21e7b1b2860
katta soumya <kattasoumya2002@gmail.com>
2024-09-29 17:41:39 +0530

    Keep the userdata in seperate file

---
+++ src/staticData/userData.ts
@@ -0,0 +1,14 @@
+ export const User: any = {
+    username: "soumya",
+    password: 123,
+    currentIncome: 25000,
+    Budget: [
+      { category: "groceries", allocated_amt: 2000, spent: 0 },
+      { category: "entertainment", allocated_amt: 4000, spent: 0 },
+      { category: "rent", allocated_amt: 6000, spent: 0 },
+    ],
+    savings: [
+      { title: "emergency fund", target_amt: 10000, current_amt_saved: 5000 },
+      { title: "trip", target_amt: 3000, current_amt_saved: 1000 },
+    ],
+  };
