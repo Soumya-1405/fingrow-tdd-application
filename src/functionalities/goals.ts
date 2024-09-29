7530b0bc0727173279d241dbf39556bd0c2ff281
katta soumya <kattasoumya2002@gmail.com>
2024-09-29 18:08:57 +0530

    Write a condition if the same goal is added again

--- src/functionalities/goals.ts
+++ src/functionalities/goals.ts
@@ -1,24 +1,11 @@
 import { User } from "../staticData/userData";
-export function addGoal(
-  title: string,
-  target_amt: number,
-  current_amt_saved: number
-) {
-  let flag = 0;
-  if (
-    User.savings.some((item: { title: string }) => {
-      if (item.title === title) {
-         
-      }
-    })
-  ) {
-    flag = 1;
-  } else {
-    User.savings.push({
-      title: title,
-      target_amt: target_amt,
-      current_amt_saved: current_amt_saved,
-    });
-    return `New goal was added successfully`;
-  }
+export function addGoal(title:string, target_amt:number, current_amt_saved:number){
+    let flag =0;
+    if ((User.savings).some((item: { title: string }) => item.title === title)) {
+       return `Goal was already added to savings goals`     
+    }
+    else{
+        (User.savings).push({title:title, target_amt:target_amt, current_amt_saved:current_amt_saved});
+        return `New goal was added successfully`
+    }
 }
