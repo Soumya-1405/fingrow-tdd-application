a974ca241bfd0ff405189e90e662b0dc6e88fdc8
katta soumya <kattasoumya2002@gmail.com>
2024-09-29 18:14:36 +0530

    Write test case to check the progress of the emergency fund

--- src/tests/goals.test.ts
+++ src/tests/goals.test.ts
@@ -7,3 +7,9 @@ describe("should check the users saving goals",()=>{
         expect(addGoal("trip",2000, 1000)).toBe(`Goal was already added to savings goals`)
     })
 })
+
+describe('should check the progress towards the goal',()=>{
+    test("should check the progress towards the emergency fund",()=>{
+        
+    })
+})
