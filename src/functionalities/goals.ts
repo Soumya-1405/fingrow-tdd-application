c9b76edd043a8603322cee5812b6b12d408e3985
katta soumya <kattasoumya2002@gmail.com>
2024-09-29 18:02:46 +0530

    Update the function by implementing logic for add new goal

--- src/functionalities/goals.ts
+++ src/functionalities/goals.ts
@@ -1,3 +1,24 @@
-export function addGoal(title:string, target_amt:number, current_amt_saved:number){
-
+import { User } from "../staticData/userData";
+export function addGoal(
+  title: string,
+  target_amt: number,
+  current_amt_saved: number
+) {
+  let flag = 0;
+  if (
+    User.savings.some((item: { title: string }) => {
+      if (item.title === title) {
+         
+      }
+    })
+  ) {
+    flag = 1;
+  } else {
+    User.savings.push({
+      title: title,
+      target_amt: target_amt,
+      current_amt_saved: current_amt_saved,
+    });
+    return `New goal was added successfully`;
+  }
 }
