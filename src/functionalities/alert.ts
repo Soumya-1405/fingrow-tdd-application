0dbb193212c01df621e40d410e3aaece78411507
katta soumya <kattasoumya2002@gmail.com>
2024-09-30 00:55:10 +0530

    Write functions  for alert when user savings are close to the targeted amount

--- src/functionalities/alert.ts
+++ src/functionalities/alert.ts
@@ -1,4 +1,14 @@
 import { User } from "../staticData/userData";
-export function alert(){
-
+export function alert(title: string) {
+  let per: any;
+  if (User.savings.some((item: { title: string }) => item.title === title)) {
+    User.savings.filter((item: any) => {
+      if (item.title == title) {
+        per = (item.current_amt_saved / item.target_amt) * 100;
+      }
+    });
+  }
+  if (per >= 80) {
+    return `hurray!! you have reached ${per} of targeted amount`;
+  }
 }
