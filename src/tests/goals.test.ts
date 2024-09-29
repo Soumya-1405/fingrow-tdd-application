d1ea6c470079b9dab11991266da4ca7512961a84
katta soumya <kattasoumya2002@gmail.com>
2024-09-29 18:08:03 +0530

    Write a test case for if the same goal is added again

--- src/tests/goals.test.ts
+++ src/tests/goals.test.ts
@@ -3,4 +3,7 @@ describe("should check the users saving goals",()=>{
     test("should able to add a saving goal",()=>{
         expect(addGoal("function",2000,1000)).toBe(`New goal was added successfully`)
     })
+    test('should able to throw a message if the sae goal was added',()=>{
+        expect(addGoal("trip",2000, 1000)).toBe(`Goal was already added to savings goals`)
+    })
 })
